403ff9717af6e83458b8d0fd17da9f3d2740d3ba
stantoha <78742661+stantoha@users.noreply.github.com>
2021-12-15 15:18:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,23 +6,23 @@ const solarSButton = document.querySelector('.solarSystem');
 console.log(buttons);
 console.log(solarSButton);
 
-solarSButton.addEventListener('click', function(){
+/* solarSButton.addEventListener('click', function(){
     solarSystem.classList.toggle('hidden');
-});
+}); */
 
 for (let i = 0; i < buttons.length ; i++) {
-    if(!(content[i].classList.contains('solar-system'))){
+    /* if(!(content[i].classList.contains('solar-system'))){ */
         buttons[i].addEventListener('click', function() {
             for(let j=0;j<content.length;j++){
                 if(j!==i){
-                    solarSystem.classList.add('hidden');
+                 /*    solarSystem.classList.add('hidden'); */
                     content[j].classList.add('hidden');
                 }else{
                     content[j].classList.toggle('hidden');
                 }
         }
     });
-}
+
 }
 
 
