1db7d528c1275b596f113a8d0da1939bdc207167
stantoha <78742661+stantoha@users.noreply.github.com>
2022-01-24 17:18:56 +0200

    Add files via upload

--- list.script.js
+++ list.script.js
@@ -1,13 +1,14 @@
-      window.addEventListener('DOMContentLoaded', ()=>{
+      window.addEventListener('DOMContentLoaded', () => {
 
-        const planetButton=document.querySelector('.nav-link','.planets'),
+            const planetButton=document.querySelector('.nav-link','.planets'),
         planets=document.querySelector('.tabheader__items'),
     tabs=document.querySelectorAll('.tabheader__link'),
     tabsContent=document.querySelectorAll('.tabcontent'),
     titleLink=document.querySelector('.title-link'),
-    upButton = document.querySelector('.up__button');
+    upButton = document.querySelector('.up__button'),
+    tabsDescrBtns = document.querySelectorAll('.tabcontent__descr__btn'),
+    tabsDescrContent = document.querySelectorAll('.tabcontent__descr');
 
-   
     planetButton.addEventListener('click',()=>{
             console.log('ciao');
             if(!planets.classList.contains('hidden')){
@@ -17,11 +18,6 @@
                 planets.classList.remove('hidden');
              }
         });
-  
-   
-        console.log(tabs);
-
-
 
     function hideTabContent(){
         tabsContent.forEach(item=>{
@@ -60,16 +56,52 @@
             }
         });
     });
-    window.onscroll = function () {
-        if (window.pageYOffset > 200) {
-          upButton.classList.remove('hidden');
-        } else{
-          upButton.classList.add('hidden');}
-      };
-      
-      upButton.onclick = function () {
-        window.scrollTo(0, 0);
-      };
+                  
+          function hideTabDescrContent() {
+              tabsDescrContent.forEach(item => {
+                  item.classList.add('hide');
+                  item.classList.remove('show', 'fade');
+              });
+              tabsDescrBtns.forEach(item => {
+                  item.classList.remove('tabcontent__descr__btn__active');
+                  item.textContent='Show more';
+              });
+          }
 
+          function showTabDescrContent(i = 0) {
+              tabsDescrContent[i].classList.add('show', 'fade');
+              tabsDescrContent[i].classList.remove('hide');
+              tabsDescrBtns[i].classList.add('tabcontent__descr__btn__active');
+              tabsDescrBtns[i].textContent='Show less';
+          }
 
-   });
+          hideTabDescrContent();
+          showTabDescrContent();
+
+          tabsDescrBtns.forEach(item => {
+              item.addEventListener('click', (event) => {
+                  const target = event.target;
+                  if (target && target.classList.contains('tabcontent__descr__btn')) {
+                      tabsDescrBtns.forEach((item, i) => {
+                          if (target == item) {
+                              hideTabDescrContent();
+                              showTabDescrContent(i);
+                          }
+                      });
+                  }
+              });
+          });
+
+          window.onscroll = function () {
+              if (window.pageYOffset > 200) {
+                  upButton.classList.remove('hidden');
+              } else {
+                  upButton.classList.add('hidden');
+              }
+          };
+
+          upButton.onclick = function () {
+              window.scrollTo(0, 0);
+          };
+
+      });
